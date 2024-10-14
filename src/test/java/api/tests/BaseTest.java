755f8aa4bb2e75442170b3cc6806870950b8593c
CEkanayake <Chathurika.Ekanayake@cambio.lk>
2024-10-14 14:48:36 +0530

    git commit -m 'add configurations to pom'

--- src/test/java/api/tests/BaseTest.java
+++ src/test/java/api/tests/BaseTest.java
@@ -1,2 +1,15 @@
-package api.tests;public class BaseTest {
+package api.tests;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.testng.annotations.BeforeSuite;
+
+public class BaseTest
+{
+  private final Logger logger = LogManager.getLogger();
+
+  @BeforeSuite
+  public void oneTimeSetup() {
+    logger.debug("Test execution environment: {}", "Pet Store");
+  }
 }
